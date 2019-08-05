481bbaa2a6a25b7e7c130b8ac1ecdac56e1e3084
Sergey Goryachev <sgoryachev@payneteasy.com>
2019-08-05 13:13:49 +0300

    add csp rule

--- src/server/config/express.js
+++ src/server/config/express.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
 const methodOverride = require('method-override');
+const csp = require('express-csp-header');
 
 const constant = require('./directory');
 
@@ -16,13 +17,17 @@ app.use(express.static(constant.distDir));
 app.use(express.static(constant.assetsDir));
 
 app.use(cors());
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-        defaultSrc: ["'self'"],
-        imgSrc: ["'self'"],
-        scriptSrc: ["'unsafe-eval'"]
+app.use(csp({
+    policies: {
+        'default-src': [csp.SELF],
+        'script-src': [csp.SELF, csp.INLINE, 'somehost.com'],
+        'style-src': [csp.SELF, 'mystyles.net'],
+        'img-src': ['data:', 'images.com'],
+        'worker-src': [csp.NONE],
+        'block-all-mixed-content': true
     }
 }));
+app.use(helmet());
 app.use(compression());
 app.use(methodOverride());
 app.use(bodyParser.json());
