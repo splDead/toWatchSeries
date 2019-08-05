ea8ba88cf0991b565014fa46ff64b8607fffb226
Sergey Goryachev <sgoryachev@payneteasy.com>
2019-08-05 13:44:44 +0300

    add csp rule

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,9 @@
+const path = require('path');
 const app = require('./config/express');
 const os = require('os');
 
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
