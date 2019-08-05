9ef4e4f0bf71ad679750e8bba94f3d8d8eb0b966
Sergey Goryachev <sgoryachev@payneteasy.com>
2019-08-05 13:53:39 +0300

    refactoring

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,39 @@
 const path = require('path');
-const app = require('./config/express');
 const os = require('os');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+const methodOverride = require('method-override');
+const csp = require(`helmet-csp`);
+
+const app = express();
+
+require('dotenv').config();
+
+app.use(express.static('dist'));
+
+app.use(cors());
+app.use(helmet());
+app.use(csp({
+    directives: {
+        defaultSrc: [`'self'`],
+        imgSrc: [`'self'`]
+    }
+}));
+app.use(compression());
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
 
 module.exports = app;
