38826ad3f505799d998c1fb101b2c703fcbd1afd
Sergey Goryachev <splD@yandex.ru>
2019-08-24 22:06:57 +0300

    ping off

--- src/server/utils/ping.js
+++ src/server/utils/ping.js
@@ -1,5 +1,5 @@
 const https = require('https');
 
-setInterval(function() {
-    https.get('https://to-watch-series.herokuapp.com');
-}, 300000); // every 5 minutes (300000)
+// setInterval(function() {
+//     https.get('https://to-watch-series.herokuapp.com');
+// }, 300000); // every 5 minutes (300000)
